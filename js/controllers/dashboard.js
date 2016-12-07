fe0b401757a8db7b411d0e34264de5ec83aa3218
gerardolsj <lsjgerardo@gmail.com>
2016-12-07 11:10:06 -0600

    changes

---
+++ js/controllers/dashboard.js
@@ -0,0 +1,11 @@
+
+  app.controller('DashboardCtrl', function($scope, $state) {
+    $scope.$state = $state;
+
+    $scope.menuItems = [];
+    angular.forEach($state.get(), function (item) {
+        if (item.data && item.data.visible) {
+            $scope.menuItems.push({name: item.name, text: item.data.text});
+        }
+    });
+  });
---
+++ js/controllers/login.js
@@ -0,0 +1,10 @@
+app.controller('LoginCtrl', function($scope, $location) {
+
+    $scope.submit = function() {
+
+      $location.path('/dashboard');
+
+      return false;
+    }
+
+  });
